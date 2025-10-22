990bd8fa4f065cbc84f3bd51b76b398454249245
Anton Bilous <oxidate@tuta.io>
2025-10-22 10:00:44 +0300

    remove quotes from schema
    there is no need to preserve case exactly as written

--- schema.sql
+++ schema.sql
@@ -1,53 +1,53 @@
-CREATE TABLE "Users" (
-    "id" SERIAL PRIMARY KEY,
-    "auth0_id" VARCHAR(256) NOT NULL UNIQUE,
-    "email" VARCHAR(256) NOT NULL UNIQUE,
-    "notify_on_expiration" BOOLEAN NOT NULL DEFAULT TRUE,
-    "notify_on_deadline" BOOLEAN NOT NULL DEFAULT TRUE,
-    "notify_on_threshold" BOOLEAN NOT NULL DEFAULT TRUE,
-    "notify_on_insufficient_resources" BOOLEAN NOT NULL DEFAULT TRUE,
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    auth0_id VARCHAR(256) NOT NULL UNIQUE,
+    email VARCHAR(256) NOT NULL UNIQUE,
+    notify_on_expiration BOOLEAN NOT NULL DEFAULT TRUE,
+    notify_on_deadline BOOLEAN NOT NULL DEFAULT TRUE,
+    notify_on_threshold BOOLEAN NOT NULL DEFAULT TRUE,
+    notify_on_insufficient_resources BOOLEAN NOT NULL DEFAULT TRUE,
 
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    "deleted_at" TIMESTAMPTZ
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ
 );
 
-CREATE TABLE "Pictures" (
-    "id" SERIAL PRIMARY KEY,
-    "blob_id" UUID NOT NULL,
-    "filetype" VARCHAR(8) NOT NULL,
+CREATE TABLE Pictures (
+    id SERIAL PRIMARY KEY,
+    blob_id UUID NOT NULL,
+    filetype VARCHAR(8) NOT NULL,
 
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE "ItemTypes" (
-    "id" SERIAL PRIMARY KEY,
+CREATE TABLE ItemTypes (
+    id SERIAL PRIMARY KEY,
 
-    "user_id" INTEGER NOT NULL,
-    "picture_id" INTEGER,
+    user_id INTEGER NOT NULL,
+    picture_id INTEGER,
 
-    "name" VARCHAR(256) NOT NULL,
-    "description" VARCHAR(512),
-    "category" VARCHAR(256),
-    "base_measurement_unit" VARCHAR(256) NOT NULL,
-    "default_quantity" REAL NOT NULL DEFAULT 1.0,
-    "width" REAL,
-    "height" REAL,
-    "depth" REAL,
+    name VARCHAR(256) NOT NULL,
+    description VARCHAR(512),
+    category VARCHAR(256),
+    base_measurement_unit VARCHAR(256) NOT NULL,
+    default_quantity REAL NOT NULL DEFAULT 1.0,
+    width REAL,
+    height REAL,
+    depth REAL,
 
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    "deleted_at" TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ,
 
-    CONSTRAINT "FK_ItemType_User"
-        FOREIGN KEY ("user_id")
-        REFERENCES "Users"("id")
+    CONSTRAINT FK_ItemType_User
+        FOREIGN KEY (user_id)
+        REFERENCES Users(id)
         ON DELETE CASCADE,
 
-    CONSTRAINT "FK_ItemType_Picture"
-        FOREIGN KEY ("picture_id")
-        REFERENCES "Pictures"("id")
+    CONSTRAINT FK_ItemType_Picture
+        FOREIGN KEY (picture_id)
+        REFERENCES Pictures(id)
         ON DELETE CASCADE
 );
 
@@ -60,11 +60,11 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON "Users"
+BEFORE UPDATE ON Users
 FOR EACH ROW
 EXECUTE FUNCTION trigger_set_timestamp();
 
 CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON "ItemTypes"
+BEFORE UPDATE ON ItemTypes
 FOR EACH ROW
 EXECUTE FUNCTION trigger_set_timestamp();
